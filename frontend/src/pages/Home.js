b1c1357d95bc36a2952f557cad6db76804e3e35b
VictorBK <victorbrian4@gmail.com>
2023-04-08 10:49:28 +0300

    Nav and Home changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Nav from '../components/Nav'
 
 const Home = () => {
+
+  const authToken = true
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
   return (
-    <div>Home</div>
+    <>
+    <Nav/>
+    <div className='home'>
+      <h1>Sweep Right®</h1>
+      <button className='primary-button' onClick={handleClick}>
+        {authToken ? 'Sign Out' : 'Create Account'}
+      </button>
+    </div>
+    </>
   )
 }
 
