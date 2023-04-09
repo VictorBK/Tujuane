cf3ad778e310aa3ac5552dab6535322af5fa2805
VictorBK <victorbrian4@gmail.com>
2023-04-09 15:30:57 +0300

    Navigation changes

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import logo from '../Images/tujuane-act-logo.png'
+import colorLogo from '../Images/color-logo.png'
 
-const Nav = () => {
+const Nav = ({ minimal}) => {
+  
   return (
-    <div>Nav</div>
+    <nav>
+      <div className="logo-container">
+        <img className='logo' src={minimal ? colorLogo : logo}/>  
+      </div>    
+    </nav>
   )
 }
 
