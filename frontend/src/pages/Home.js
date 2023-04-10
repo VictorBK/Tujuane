facefc5edf5129998b95074644f5a94196568df1
VictorBK <victorbrian4@gmail.com>
2023-04-10 19:22:51 +0300

    Homepage alterations

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,21 +3,21 @@ import Nav from '../components/Nav'
 
 const Home = () => {
 
-  const authToken = true
+  const authToken = false
 
   const handleClick = () => {
     console.log('clicked')
   }
   return (
-    <>
-    <Nav minimal={false}/>
+    <div className='overlay'>
+    <Nav minimal={false} authToken={authToken}/>
     <div className='home'>
       <h1>Sweep Right®</h1>
       <button className='primary-button' onClick={handleClick}>
         {authToken ? 'Sign Out' : 'Create Account'}
       </button>
     </div>
-    </>
+    </div>
   )
 }
 
