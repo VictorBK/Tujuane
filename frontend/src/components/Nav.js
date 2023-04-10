0b7071518a37cc6723009c7118eb4aa1563a5f1a
VictorBK <victorbrian4@gmail.com>
2023-04-10 19:21:50 +0300

    authToken change

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import logo from '../Images/tujuane-act-logo.png'
+import logo from '../Images/logo-for-tujuane.png'
 import colorLogo from '../Images/color-logo.png'
 
-const Nav = ({ minimal}) => {
+const Nav = ({ minimal, authToken }) => {
   
   return (
     <nav>
       <div className="logo-container">
         <img className='logo' src={minimal ? colorLogo : logo}/>  
-      </div>    
+      </div>
+       
+      {!authToken && !minimal && <button className="nav-button">Log in</button>}   
     </nav>
   )
 }
