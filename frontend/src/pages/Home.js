e77ed074c262e31339c1b6496f12905fe8cf39b2
VictorBK <victorbrian4@gmail.com>
2023-04-11 21:41:35 +0300

    Home draft changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import AuthModal from '../components/AuthModal'
+import { useState } from 'react'
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false)
 
   const authToken = false
 
   const handleClick = () => {
     console.log('clicked')
+    setShowModal(true)
   }
   return (
     <div className='overlay'>
@@ -16,6 +20,10 @@ const Home = () => {
       <button className='primary-button' onClick={handleClick}>
         {authToken ? 'Sign Out' : 'Create Account'}
       </button>
+
+      {showModal && (
+        <AuthModal setShowModal={setShowModal}/>
+      )}
     </div>
     </div>
   )
