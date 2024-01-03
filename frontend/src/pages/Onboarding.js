3e55e69130ad4a90badc5dc40513096a20367902
VictorBK <victorbrian4@gmail.com>
2024-01-03 16:02:26 +0300

    DOB and gender labels initiated

--- frontend/src/pages/Onboarding.js
+++ frontend/src/pages/Onboarding.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+/* import { useState } from 'react'; */
 import Nav from "../components/Nav";
 
 const Onboarding = () => {
@@ -8,9 +8,89 @@ const Onboarding = () => {
         <Nav
             minimal={true}
             setShowModal={() => {}}
-            showModal={false}       
+            showModal={false}     
         />
-        <div>
+        <div className='onboarding'>
+          <h2>Create Account</h2>
+
+          <form onSubmit={handleSubmit}>
+            <section>
+              <label htmlFor="first_name">First Name</label>
+              <input
+                  id="first_name"
+                  type="text"
+                  name="first_name"
+                  placeholder="First Name"
+                  required={true}
+                  value={""}
+                  onChange={handleChange}              
+              />
+
+              <label>Birthday</label>
+              <div className='multiple-input-container'>
+              <input
+                  id="dob_day"
+                  type="number"
+                  name="dob_day"
+                  placeholder="DD"
+                  required={true}
+                  value={""}
+                  onChange={handleChange}              
+              />
+              <input
+                  id="dob_month"
+                  type="number"
+                  name="dob_month"
+                  placeholder="MM"
+                  required={true}
+                  value={""}
+                  onChange={handleChange}
+              />
+
+              <input
+                  id="dob_year"
+                  type="number"
+                  name="dob_year"
+                  placeholder="YYYY"
+                  required={true}
+                  value={""}
+                  onChange={handleChange}
+              />
+              </div>
+
+              <label>Gender</label>
+              <div className="multiple-input-container">
+              <input
+                  id="man-gender-identity"
+                  type="radio"
+                  name="gender_identity"
+                  value="man"
+                  onChange={handleChange}
+                  checked={false}
+              />
+              <label htmlFor='woman-gender-identity'>Woman</label>
+              <input
+                  id="woman-gender-identity"
+                  type="radio"
+                  name="gender_identity"
+                  value="woman"
+                  onChange={handleChange}
+                  checked={false}
+              />
+
+              <label htmlFor='more-gender-identity'>More</label>
+              <input
+                  id="more-gender-identity"
+                  type="radio"
+                  name="gender_identity"
+                  value="more"
+                  onChange={handleChange}
+                  checked={false}
+              />
+              </div>              
+            </section>
+
+          </form>
 
         </div>
     </>
